4828dd326108d62b3979550360275aefb414e4bc
Jake Gealer <jake@gealer.email>
2021-03-19 14:31:57 +0000

    Switch application from Parcel to Webpack (#29)
    * move to webpack
    
    * use .css
    
    * bump do-vue
    
    * Bump to do-vue@https://github.com/do-community/do-vue.git#f13494d7a58ba8f00d121e15bc4fcbbc36234701
    
    * Bump to do-vue@https://github.com/do-community/do-vue.git#869ac289fb02195a90ee70402ed97bd3a008905b
    
    * Bump to do-vue@https://github.com/do-community/do-vue.git#7ba6df2241e36635a6137b873620f026ba45fe93
    
    * Fix dev command
    
    * Bump to do-vue@https://github.com/do-community/do-vue.git
    
    Co-authored-by: MattIPv4 <me@mattcowley.co.uk>

--- test/mount.js
+++ test/mount.js
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+import "./scss/style"
+
 import Vue from "vue"
 import App from "./templates/app.vue"
 
